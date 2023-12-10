4002f777dac6a4f44b45b23e08c2517a445ecbae
Sergei O. Udalov <udalov.x@mail.ru>
2023-12-10 19:13:24 +0300

    patch_04 19:13

--- lib/examples.rb
+++ lib/examples.rb
@@ -6,14 +6,20 @@ require 'division'
 
 class Examples
   def all
-    arr = []
+    multiplications = []
+    divisions = []
+
     (2..9).each do |a|
       (2..9).each do |b|
-        arr << Example.new(Multiplication.new(a, b))
-        arr << Example.new(Division.new(a * b, a))
+        multiplications << Example.new(Multiplication.new(a, b))
+        divisions << Example.new(Division.new(a * b, a))
       end
     end
 
-    arr.sort_by { rand }.take(20)
+    arr1 = multiplications.sort_by { rand }.take(10)
+    arr2 = divisions.sort_by { rand }.take(10)
+
+    (arr1 + arr2)
   end
+
 end
